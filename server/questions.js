d4593e57ec9b2562bd33cdd00edfb6485196c84b
Jens Christian Hillerup <jens@hillerup.net>
2012-04-25 09:42:02 +0200

    make sure that groups cannot answer when the answers are being shown on screen

---
+++ server/questions.js
@@ -0,0 +1,11 @@
+var questions = [
+    {
+        "question": "Hvor gammel bliver en ræv?",
+        "answers": {            
+            "A": "5-6 år",
+            "B": "10-12 år",
+            "C": "17-18 år"
+        },
+        "correct_answer": "B"
+    }
+];
