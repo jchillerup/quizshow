cbf3bfd9d6f692eb922a56ac52c2f828b29c80fa
Jens Christian Hillerup <jens@hillerup.net>
2012-04-25 09:20:15 +0200

    initial commit

---
+++ display/js/client.js
@@ -0,0 +1,42 @@
+var socket = io.connect('http://kodekode.dk:8081');
+
+socket.on('yes', function (data) {
+              renderState(data);
+          });
+
+
+var renderState = function(state) {
+
+    for (var i = 0; i<=state.teamAnswers.length; i++) {
+        var $curDOMID = $group(i+1);
+        $curDOMID.removeClass('answered correct incorrect');
+        
+        var newClass = '';
+        
+        if (state.answersPublic === false) {
+            if (state.teamAnswers[i] !== 'X') {
+                newClass = 'answered';
+            } else {
+                newClass = '';
+            }
+        } else {
+            switch(state.teamAnswers[i]) {
+            case 'A':
+                newClass = 'correct';
+                break;
+            case 'B':
+                newClass = 'incorrect';
+                break;
+            case 'C':
+                newClass = 'incorrect';
+                break;
+            }
+        }            
+        
+        $curDOMID.addClass(newClass);
+    }
+};
+
+var $group = function(id) {
+    return $("#group"+id);
+};
---
+++ server/server.js
@@ -0,0 +1,40 @@
+// Quizshow server
+var io = require('socket.io').listen(8081);
+var http = require('http');
+
+var state = {
+    answersPublic: false,
+    numTeams: 8,
+    teamAnswers: ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X']
+};
+
+io.sockets.on('connection', function(socket) {
+                  socket.emit('yes', state);
+              });
+
+var processRequest = function(url) {
+    fields = url.split('/');
+    fields.shift(); // shift away the first slash
+
+    if(fields[0] === 'show') {
+        state.answersPublic = true;
+    } else if(fields[0] === 'reset') {
+        state.answersPublic = false;
+        for(i=0; i<state.teamAnswers.length; i++){
+            state.teamAnswers[i] = 'X';
+        }
+    } else {
+        var team = fields[0];
+        var answer = fields[1];
+
+        state.teamAnswers[team-1] = answer;
+    }
+
+    return state;
+};
+
+http.createServer(function(request, response) {
+			response.writeHead(200, {"Content-Type": "text/plain"});
+			response.end("OK");
+			io.sockets.emit('yes', processRequest(request.url));
+}).listen(8080);
