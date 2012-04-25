e7548cd5132cc4ceb5b2459bd0d62e3c056b5144
Jens Christian Hillerup <jens@hillerup.net>
2012-04-25 12:33:36 +0200

    proper questions

--- server/questions.js
+++ server/questions.js
@@ -1,29 +1,92 @@
 exports.questions = [
     {
-        "question": "Hvor gammel bliver en ræv?",
+        "question": "Hvilke af følgende discipliner er ikke anset som en del af mekatronik?",
         "answers": {            
-            "A": "5-6 år",
-            "B": "10-12 år",
-            "C": "17-18 år"
+            "A": "Mekanik",
+            "B": "Management",
+            "C": "Software"
         },
         "correct_answer": "B"
     },
     {
-        "question": "Hvor gammel er Ali?",
+        "question": "Hvilken sensortype vil være mest oplagt at bruge til at måle hvor langt et hjul har drejet?",
         "answers": {            
-            "A": "5-6 år",
-            "B": "10-12 år",
-            "C": "17-18 år"
+            "A": "Potentiometer",
+            "B": "Sonar",
+            "C": "Encoder"
         },
         "correct_answer": "C"
     },
     {
-        "question": "?",
+        "question": "Hvad er tallet 12 oversat til binært?",
         "answers": {            
-            "A": "5-6 år",
-            "B": "10-12 år",
-            "C": "17-18 år"
+            "A": "1100",
+            "B": "1010",
+            "C": "1001"
+        },
+        "correct_answer": "A"
+    },
+    {
+        "question": "Hvad repræsenterer en cirkel i et state-diagram?",
+        "answers": {            
+            "A": "en transition",
+            "B": "et output",
+            "C": "et state"
+        },
+        "correct_answer": "C"
+    },
+    {
+        "question": "Hvilke værdier skal Z og X have for at følgende udtryk er sandt?: ((X==4) OR (4==10) AND (Z>X))",
+        "answers": {            
+            "A": "Z<X",
+            "B": "X = 4",
+            "C": "X = 10, Z > X"
         },
         "correct_answer": "B"
+    },
+    {
+        "question": "Hvilken sensortype er en normal knap?",
+        "answers": {            
+            "A": "Digital",
+            "B": "Analog",
+            "C": "Magi"
+        },
+        "correct_answer": "A"
+    },
+    {
+        "question": "Prins Ali vil interface med en gammel solarieseng han har købt på et loppemarked på Falster. Hvilken komponent vil være oplagt at bruge til at tænde og slukke for sengen?",
+        "answers": {            
+            "A": "Transistor",
+            "B": "MOSFET",
+            "C": "Relæ"
+        },
+        "correct_answer": "C"
+    },
+    {
+        "question": "Prins Ali vil gerne styre en 10 Volt LED der suger 2 ampere. Hvilken komponent ville være oplagt at bruge til at PWMe LEDen?",
+        "answers": {            
+            "A": "Transistor",
+            "B": "MOSFET",
+            "C": "Relæ"
+        },
+        "correct_answer": "B"
+    },
+    {
+        "question": "Hvad vil følgende for-loop skrive ud?: for(int i = 0; i<10;i+=2){Serial.print(i/2);}",
+        "answers": {            
+            "A": "012345",
+            "B": "01234",
+            "C": "12345"
+        },
+        "correct_answer": "B"
+    },
+    {
+        "question": "Hvad er outputtet af en normal DC-motor?",
+        "answers": {            
+            "A": "Moment",
+            "B": "Kraft",
+            "C": "Hastighed"
+        },
+        "correct_answer": "A"
     }
 ];
