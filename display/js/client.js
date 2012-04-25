061264609895e02387c24bc315959ee54edd4a8b
Jens Christian Hillerup <jens@hillerup.net>
2012-04-25 12:48:33 +0200

    view code

--- display/js/client.js
+++ display/js/client.js
@@ -8,21 +8,25 @@ socket.on('yes', function (data) {
 var renderState = function(state) {
     console.log(state);   
 
+    if (state.curQuestion !== null) {
+        $("#questionContainer").show();
+        $("#question").html(state.curQuestion.question);
+        $("#questionA").html(state.curQuestion.answers.A);
+        $("#questionB").html(state.curQuestion.answers.B);
+        $("#questionC").html(state.curQuestion.answers.C);
+    } else {
+        $("#questionContainer").hide();
+    }
+    
+    for (var i = 0; i<state.teamPoints; i++) {
+        $group(i+1).children('.points').html(state.teamPoints[i]);
+    }
+
     for (var i = 0; i<=state.teamAnswers.length; i++) {
         var $curDOMID = $group(i+1);
         $curDOMID.removeClass('answered correct incorrect');
         
         var newClass = '';
-        
-        if (state.curQuestion !== null) {
-            $("#questionContainer").show();
-            $("#question").html(state.curQuestion.question);
-            $("#questionA").html(state.curQuestion.answers.A);
-            $("#questionB").html(state.curQuestion.answers.B);
-            $("#questionC").html(state.curQuestion.answers.C);
-        } else {
-            $("#questionContainer").hide();
-        }
         
         if (state.answersPublic === false) {
             $(".answerContainer span").removeClass('correct');
