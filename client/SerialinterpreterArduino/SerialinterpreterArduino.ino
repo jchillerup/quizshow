ac3b1d5ca0aa9103cc0c95603484eb3b6637852f
Jens Christian Hillerup <jens@hillerup.net>
2012-04-26 13:37:33 +0200

    added client arduino and processing sketches. still some bugs in the server to be fixed

---
+++ client/SerialinterpreterArduino/SerialinterpreterArduino.ino
@@ -0,0 +1,61 @@
+
+/* Matti og JCs Quizshow
+
+Ret jeres Gruppenummer i integeren groupnumber.
+Knappe (med pull up resitor) i portene 2, 3 og 4.
+
+*/
+int groupnumber = 0;
+long lastpress = 0;
+int inByte = 0;         // incoming serial byte
+char answer = '0';
+
+void setup()
+{
+  // start serial port at 9600 bps:
+  Serial.begin(9600);
+  pinMode(2, INPUT);   // digital sensore
+  pinMode(3, INPUT);   
+  pinMode(4, INPUT);   
+ 
+  establishContact();  // send a byte to establish contact until Processing responds 
+}
+
+void loop()
+{
+  // if we get a valid byte, read analog ins:
+  if (Serial.available() > 0) {
+    // get incoming byte:
+    inByte = Serial.read();
+   
+   }
+   if(millis()-lastpress>200){
+   if(digitalRead(4) == LOW){
+     answer = 'C';
+    Serial.write(groupnumber);
+    Serial.write(answer);
+    lastpress = millis();    
+   }
+   else if(digitalRead(3) == LOW){
+     answer = 'B';
+     Serial.write(groupnumber);
+    Serial.write(answer);      
+    lastpress = millis();
+ }
+   else if(digitalRead(2) == LOW){
+     answer = 'A';
+     Serial.write(groupnumber);
+    Serial.write(answer);      
+    lastpress = millis();
+ }
+   }           
+  }
+
+
+void establishContact() {
+ while (Serial.available() <= 0) {
+      Serial.write('A');   // send a capital A
+      delay(300);
+  }
+}
+
